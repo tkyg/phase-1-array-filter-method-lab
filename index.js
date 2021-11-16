09b214f0cdd7c0fe48241cfc4e00178cc33f1416
tkyg <80114433+tkyg@users.noreply.github.com>
2021-11-16 16:59:30 -0500

    commit tree af84555cc1c195b934adcc2470e61834cfaea0bf

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+function findMatching(drivers, string){
+    return drivers.filter( driver => driver.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter(function(sa){
+        return sa.startsWith(string)
+    })
+}
+
+function matchName(drivers, name){
+    return drivers.filter (function(driver){
+        return driver.name === name
+    })
+}
+    
